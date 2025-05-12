5fd9ac421636f866ed0fae53fa8888e296c4bd8f
James Britton <james.britton@rocksolidknowledge.com>
2025-05-12 15:13:05 +0100

    fix: various warning on key rotation sample

--- Samples.KeyRotationWithEntityFramework/Controllers/UserinfoController.cs
+++ Samples.KeyRotationWithEntityFramework/Controllers/UserinfoController.cs
@@ -9,27 +9,21 @@ using static OpenIddict.Abstractions.OpenIddictConstants;
 
 namespace Samples.KeyRotationWithEntityFramework.Controllers;
 
-public class UserinfoController : Controller
+public class UserinfoController(UserManager<ApplicationUser> userManager) : Controller
 {
-    private readonly UserManager<ApplicationUser> _userManager;
-
-    public UserinfoController(UserManager<ApplicationUser> userManager)
-    {
-        _userManager = userManager;
-    }    
-
-
     // GET: /api/userinfo
     [Authorize(AuthenticationSchemes = OpenIddictServerAspNetCoreDefaults.AuthenticationScheme)]
     [HttpGet("~/connect/userinfo"), HttpPost("~/connect/userinfo"), Produces("application/json")]
     public async Task<IActionResult> Userinfo()
     {
-        var user = await _userManager.FindByIdAsync(User.GetClaim(Claims.Subject));
+        var subject = User.GetClaim(Claims.Subject) ?? 
+                      throw new InvalidOperationException("Unable to get subject from ClaimsPrinciple");
+        var user = await userManager.FindByIdAsync(subject);
         if (user is null)
         {
             return Challenge(
                 authenticationSchemes: OpenIddictServerAspNetCoreDefaults.AuthenticationScheme,
-                properties: new AuthenticationProperties(new Dictionary<string, string>
+                properties: new AuthenticationProperties(new Dictionary<string, string?>
                 {
                     [OpenIddictServerAspNetCoreConstants.Properties.Error] = Errors.InvalidToken,
                     [OpenIddictServerAspNetCoreConstants.Properties.ErrorDescription] =
@@ -40,24 +34,26 @@ public class UserinfoController : Controller
         var claims = new Dictionary<string, object>(StringComparer.Ordinal)
         {
             // Note: the "sub" claim is a mandatory claim and must be included in the JSON response.
-            [Claims.Subject] = await _userManager.GetUserIdAsync(user)
+            [Claims.Subject] = await userManager.GetUserIdAsync(user)
         };
 
         if (User.HasScope(Scopes.Email))
         {
-            claims[Claims.Email] = await _userManager.GetEmailAsync(user);
-            claims[Claims.EmailVerified] = await _userManager.IsEmailConfirmedAsync(user);
+            claims[Claims.Email] = await userManager.GetEmailAsync(user) ?? 
+                                   throw new InvalidOperationException($"Failed to get email from user with subject '{subject}'");
+            claims[Claims.EmailVerified] = await userManager.IsEmailConfirmedAsync(user);
         }
 
         if (User.HasScope(Scopes.Phone))
         {
-            claims[Claims.PhoneNumber] = await _userManager.GetPhoneNumberAsync(user);
-            claims[Claims.PhoneNumberVerified] = await _userManager.IsPhoneNumberConfirmedAsync(user);
+            claims[Claims.PhoneNumber] = await userManager.GetPhoneNumberAsync(user) ?? 
+                                         throw new InvalidOperationException($"Failed to get phone number from user with subject '{subject}'");
+            claims[Claims.PhoneNumberVerified] = await userManager.IsPhoneNumberConfirmedAsync(user);
         }
 
         if (User.HasScope(Scopes.Roles))
         {
-            claims[Claims.Role] = await _userManager.GetRolesAsync(user);
+            claims[Claims.Role] = await userManager.GetRolesAsync(user);
         }
 
         // Note: the complete list of standard claims supported by the OpenID Connect specification
